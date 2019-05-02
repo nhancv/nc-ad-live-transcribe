6c99ef7ea1bdebc17150488f95768fe71ab957bd
Nhan Cao <nhancv92@gmail.com>
2019-05-02 18:26:56 +0700

    add speech service

--- app/src/main/java/com/nhancv/livetranscribe/MainActivity.java
+++ app/src/main/java/com/nhancv/livetranscribe/MainActivity.java
@@ -1,33 +1,147 @@
 package com.nhancv.livetranscribe;
 
-import android.os.Handler;
-import android.os.HandlerThread;
-import android.support.v7.app.AppCompatActivity;
+import android.Manifest;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
-import java.io.IOException;
+public class MainActivity extends AppCompatActivity implements MessageDialogFragment.Listener {
 
-public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private static final int REQUEST_RECORD_AUDIO_PERMISSION = 1;
 
+    private final VoiceRecorder.Callback voiceCallback = new VoiceRecorder.Callback() {
+
+        @Override
+        public void onVoiceStart() {
+            showStatus(true);
+            if (isServiceReady()) {
+                speedRecognizeService.getSpeedRecognize().start(voiceRecorder.getSampleRate());
+            }
+        }
+
+        @Override
+        public void onVoice(byte[] data, int size) {
+            if (isServiceReady()) {
+                speedRecognizeService.getSpeedRecognize().streamingMicRecognize(data, size);
+            }
+        }
+
+        @Override
+        public void onVoiceEnd() {
+            showStatus(false);
+            if (isServiceReady()) {
+                speedRecognizeService.getSpeedRecognize().stop();
+            }
+        }
+    };
+    private final SpeedRecognize.Listener speechServiceListener = new SpeedRecognize.Listener() {
+        @Override
+        public void onSpeechRecognized(String text, boolean isFinal) {
+            if (isFinal) {
+                voiceRecorder.dismiss();
+            }
+            Log.e(TAG, "onSpeechRecognized: " + text + " isFinal: " + isFinal);
+        }
+    };
+    private final ServiceConnection serviceConnection = new ServiceConnection() {
+
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder binder) {
+            speedRecognizeService = SpeedRecognizeService.from(binder);
+            speedRecognizeService.getSpeedRecognize().addListener(speechServiceListener);
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            speedRecognizeService = null;
+        }
+
+    };
+
+    private VoiceRecorder voiceRecorder;
+    private SpeedRecognizeService speedRecognizeService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        HandlerThread handlerThread = new HandlerThread("Speed Recognize");
-        handlerThread.start();
-        Handler handler = new Handler(handlerThread.getLooper());
-        handler.post(new Runnable() {
-            @Override
-            public void run() {
-                SpeedRecognize speedRecognize = new SpeedRecognize();
-                try {
-                    speedRecognize.test(MainActivity.this);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        // Prepare Cloud Speech API
+        bindService(new Intent(this, SpeedRecognizeService.class), serviceConnection, BIND_AUTO_CREATE);
+
+        // Start listening to voices
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO)
+                == PackageManager.PERMISSION_GRANTED) {
+            startVoiceRecorder();
+        } else if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.RECORD_AUDIO)) {
+            showPermissionMessageDialog();
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO},
+                    REQUEST_RECORD_AUDIO_PERMISSION);
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        // Stop listening to voice
+        stopVoiceRecorder();
+
+        // Stop Cloud Speech API
+        if (speedRecognizeService != null) {
+            speedRecognizeService.getSpeedRecognize().removeListener(speechServiceListener);
+        }
+        unbindService(serviceConnection);
+        speedRecognizeService = null;
+
+        super.onStop();
+    }
+
+
+    private void startVoiceRecorder() {
+        if (voiceRecorder != null) {
+            voiceRecorder.stop();
+        }
+        voiceRecorder = new VoiceRecorder(voiceCallback);
+        voiceRecorder.start();
+    }
+
+    private void stopVoiceRecorder() {
+        if (voiceRecorder != null) {
+            voiceRecorder.stop();
+            voiceRecorder = null;
+        }
+    }
+
+    public boolean isServiceReady() {
+        return speedRecognizeService != null && speedRecognizeService.getSpeedRecognize() != null;
+    }
+
+    public void showStatus(boolean status) {
+        Log.e(TAG, "showStatus: " + status);
+    }
+
+    private void showPermissionMessageDialog() {
+        MessageDialogFragment
+                .newInstance("This app needs to record audio and recognize your speech.")
+                .show(getSupportFragmentManager(), "message_dialog");
+    }
+
+    @Override
+    public void onMessageDialogDismissed() {
+
     }
 }
--- app/src/main/java/com/nhancv/livetranscribe/SpeedRecognize.java
+++ app/src/main/java/com/nhancv/livetranscribe/SpeedRecognize.java
@@ -1,30 +1,175 @@
 package com.nhancv.livetranscribe;
 
-import android.content.Context;
-import android.util.Log;
+import android.support.annotation.NonNull;
 
 import com.google.api.gax.core.FixedCredentialsProvider;
-import com.google.api.gax.rpc.BidiStream;
+import com.google.api.gax.core.FixedExecutorProvider;
+import com.google.api.gax.rpc.ClientStream;
+import com.google.api.gax.rpc.ResponseObserver;
+import com.google.api.gax.rpc.StreamController;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.speech.v1.RecognitionAudio;
 import com.google.cloud.speech.v1.RecognitionConfig;
+import com.google.cloud.speech.v1.RecognizeResponse;
 import com.google.cloud.speech.v1.SpeechClient;
+import com.google.cloud.speech.v1.SpeechRecognitionAlternative;
+import com.google.cloud.speech.v1.SpeechRecognitionResult;
 import com.google.cloud.speech.v1.SpeechSettings;
+import com.google.cloud.speech.v1.StreamingRecognitionConfig;
+import com.google.cloud.speech.v1.StreamingRecognitionResult;
 import com.google.cloud.speech.v1.StreamingRecognizeRequest;
 import com.google.cloud.speech.v1.StreamingRecognizeResponse;
 import com.google.protobuf.ByteString;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.concurrent.ExecutionException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.Executors;
 
 public class SpeedRecognize {
     private static final String TAG = SpeedRecognize.class.getSimpleName();
 
-    public void test(Context context) throws IOException {
+    private static final int SAMPLE_RATE = 16000;
+    private ArrayList<Listener> listeners;
+    private ResponseObserver<StreamingRecognizeResponse> responseObserver;
+    private SpeechSettings speechSettings;
+    private SpeechClient speechClient;
+    private ClientStream<StreamingRecognizeRequest> clientStream;
 
+    public SpeedRecognize(InputStream credentialsStream) {
+        try {
+            // Instantiates a client
+            GoogleCredentials credentials = GoogleCredentials.fromStream(credentialsStream);
+            FixedCredentialsProvider credentialsProvider = FixedCredentialsProvider.create(credentials);
+            speechSettings =
+                    SpeechSettings.newBuilder()
+                            .setCredentialsProvider(credentialsProvider)
+                            .setExecutorProvider(FixedExecutorProvider.create(Executors.newScheduledThreadPool(
+                                    Math.max(2, Math.min(Runtime.getRuntime().availableProcessors() - 1, 4)))))
+                            .build();
+            speechClient = SpeechClient.create(speechSettings);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        listeners = new ArrayList<>();
+        responseObserver = new ResponseObserver<StreamingRecognizeResponse>() {
+
+            @Override
+            public void onStart(StreamController controller) {
+            }
+
+            @Override
+            public void onResponse(StreamingRecognizeResponse response) {
+                List<StreamingRecognitionResult> resultList = response.getResultsList();
+                if (resultList != null && !resultList.isEmpty()) {
+                    StreamingRecognitionResult result = response.getResultsList().get(0);
+                    SpeechRecognitionAlternative alternative = result.getAlternativesList().get(0);
+                    for (Listener listener : listeners) {
+                        listener.onSpeechRecognized(alternative.getTranscript(), result.getIsFinal());
+                    }
+                }
+
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                t.printStackTrace();
+            }
+        };
+    }
+
+    public void addListener(@NonNull Listener listener) {
+        listeners.add(listener);
+    }
+
+    public void removeListener(@NonNull Listener listener) {
+        listeners.remove(listener);
+    }
+
+    public void start() {
+        start(SAMPLE_RATE);
+    }
+
+    public void start(int sampleRateHertz) {
+        if (speechClient != null && !speechClient.isShutdown()) {
+            clientStream =
+                    speechClient.streamingRecognizeCallable().splitCall(responseObserver);
+            RecognitionConfig recognitionConfig =
+                    RecognitionConfig.newBuilder()
+                            .setEncoding(RecognitionConfig.AudioEncoding.LINEAR16)
+                            .setLanguageCode("en-US")
+                            .setSampleRateHertz(sampleRateHertz)
+                            .build();
+            StreamingRecognitionConfig streamingRecognitionConfig =
+                    StreamingRecognitionConfig.newBuilder().setConfig(recognitionConfig).build();
+
+            StreamingRecognizeRequest request =
+                    StreamingRecognizeRequest.newBuilder()
+                            .setStreamingConfig(streamingRecognitionConfig)
+                            .build();
+            clientStream.send(request);
+        }
+    }
+
+    public void stop() {
+        if (responseObserver != null) {
+            responseObserver.onComplete();
+        }
+        if (clientStream != null) {
+            clientStream.closeSend();
+        }
+    }
+
+    public void destroy() {
+        if (speechClient != null && !speechClient.isShutdown()) {
+            speechClient.shutdown();
+        }
+    }
+
+    /**
+     * Performs microphone streaming speech recognition with a duration of 1 minute.
+     */
+    public void streamingMicRecognize(byte[] data, int size) {
+        try {
+            StreamingRecognizeRequest request =
+                    StreamingRecognizeRequest.newBuilder()
+                            .setAudioContent(ByteString.copyFrom(data, 0, size))
+                            .build();
+            clientStream.send(request);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        responseObserver.onComplete();
+    }
+
+    public interface Listener {
+
+        /**
+         * Called when a new piece of text was recognized by the Speech API.
+         *
+         * @param text The text.
+         * @param isFinal {@code true} when the API finished processing audio.
+         */
+        void onSpeechRecognized(String text, boolean isFinal);
+
+    }
+
+    /**
+     * Synchronous speech recognition returns the recognized text for short audio (less than ~1 minute) in the response as soon as it is processed.
+     * https://cloud.google.com/speech-to-text/docs/sync-recognize
+     * InputStream credentialsStream = context.getResources().openRawResource(R.raw.credentials);
+     * InputStream inputStream = context.getResources().openRawResource(R.raw.audio);
+     */
+    public static void recognizeShortAudioFile(InputStream credentialsStream, InputStream rawAudioFileInputStream, Listener listener) throws IOException {
         // Instantiates a client
-        InputStream credentialsStream = context.getResources().openRawResource(R.raw.credentials);
         GoogleCredentials credentials = GoogleCredentials.fromStream(credentialsStream);
         FixedCredentialsProvider credentialsProvider = FixedCredentialsProvider.create(credentials);
 
@@ -35,10 +180,6 @@ public class SpeedRecognize {
 
         // Instantiates a client
         try (SpeechClient speechClient = SpeechClient.create(speechSettings)) {
-
-            // Reads the audio file into memory
-            InputStream inputStream = context.getResources().openRawResource(R.raw.audio);
-
             // Builds the sync recognize request
             RecognitionConfig config = RecognitionConfig.newBuilder()
                     .setEncoding(RecognitionConfig.AudioEncoding.LINEAR16)
@@ -46,45 +187,21 @@ public class SpeedRecognize {
                     .setLanguageCode("en-US")
                     .build();
             RecognitionAudio audio = RecognitionAudio.newBuilder()
-                    .setContent(ByteString.readFrom(inputStream))
+                    .setContent(ByteString.readFrom(rawAudioFileInputStream))
                     .build();
 
             // Performs speech recognition on the audio file
-//        RecognizeResponse response = speechClient.recognize(config, audio);
-//        List<SpeechRecognitionResult> results = response.getResultsList();
-//
-//        for (SpeechRecognitionResult result : results) {
-//            // There can be several alternative transcripts for a given chunk of speech. Just use the
-//            // first (most likely) one here.
-//            SpeechRecognitionAlternative alternative = result.getAlternativesList().get(0);
-//            System.out.printf("Transcription: %s%n", alternative.getTranscript());
-//        }
+            RecognizeResponse response = speechClient.recognize(config, audio);
+            List<SpeechRecognitionResult> results = response.getResultsList();
 
-//            LongRunningRecognizeRequest request = LongRunningRecognizeRequest.newBuilder()
-//                    .setConfig(config)
-//                    .setAudio(audio)
-//                    .build();
-            Log.e(TAG, "test: 0");
-//            LongRunningRecognizeResponse response = speechClient.longRunningRecognizeAsync(request).get();
-
-//            for (SpeechRecognitionResult result : response.getResultsList()) {
-//                SpeechRecognitionAlternative alternative = result.getAlternativesList().get(0);
-//                Log.e(TAG, String.format("Transcription: %s%n", alternative.getTranscript()));
-//            }
-
-
-            BidiStream<StreamingRecognizeRequest, StreamingRecognizeResponse> bidiStream =
-                    speechClient.streamingRecognizeCallable().call();
-
-            StreamingRecognizeRequest request = StreamingRecognizeRequest.newBuilder().build();
-            bidiStream.send(request);
-            for (StreamingRecognizeResponse response : bidiStream) {
-                // Do something when receive a response
+            for (int i = 0; i < results.size(); i++) {
+                SpeechRecognitionResult result = results.get(i);
+                SpeechRecognitionAlternative alternative = result.getAlternativesList().get(0);
+                if (listener != null) {
+                    listener.onSpeechRecognized(alternative.getTranscript(), true);
+                }
             }
-
-
-            Log.e(TAG, "test: 1");
-
         }
     }
+
 }
